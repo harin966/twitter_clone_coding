20c005c997b8f76f3f78677a10c4a7128f1b06ad
harin966 <harin966@naver.com>
2024-09-17 01:03:59 +0900

    [Twitter clone] routing
    - routing page 추가
    - outlet 을 통한 layout 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,54 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyle = createGlobalStyle`
+${reset};
+  *{
+    box-sizing: border-box;}
+body {
+  background-color: black;
+  color: white;
+  font-family: sysetm-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+}`;
 
 function App() {
-  return <></>
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/routes/create-account.tsx
@@ -0,0 +1,3 @@
+export default function createAccount() {
+  return <h1>Create Account</h1>;
+}
